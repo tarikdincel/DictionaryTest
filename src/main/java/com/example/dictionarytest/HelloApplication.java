18c0732d3c5982f9271192fc4fffa9970c6e7f5f
tarikdincel <126758735+tarikdincel@users.noreply.github.com>
2023-05-06 12:20:28 +0300

    Gui progress

--- src/main/java/com/example/dictionarytest/HelloApplication.java
+++ src/main/java/com/example/dictionarytest/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("DictGui.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 650);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
         stage.setTitle("Offline Dictionary");
 
         stage.setScene(scene);
